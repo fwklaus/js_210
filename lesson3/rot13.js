3f44a5d1e5cecc2dda5740caf1b53ace7ef434e7
Forrest Klausmeier <fwklausmeier@gmail.com>
2022-10-24 12:31:41 -0600

    Lesson 3: Exercises

--- lesson3/rot13.js
+++ lesson3/rot13.js
@@ -1,3 +1,31 @@
+// const UPPERCASE_ASCII_MID = 78;
+// const LOWERCASE_ASCII_MID = 110;
+// const ALPHABET_LENGTH = 26;
+// const ROTATION = 13;
+
+// function rot13(string) {
+//   let stringArr = [];
+
+//   string.split('').forEach(function(char){
+//     let currCharASCII = char.charCodeAt();
+//     let overflowASCII;
+
+//     if (char.match(/[A-Z]/) && currCharASCII >= UPPERCASE_ASCII_MID) { 
+//         overflowASCII = (currCharASCII - ALPHABET_LENGTH) + ROTATION;
+//         char = String.fromCharCode(overflowASCII);
+//       } else if (char.match(/[a-z]/) && currCharASCII >= LOWERCASE_ASCII_MID) { 
+//         overflowASCII = (currCharASCII - ALPHABET_LENGTH) + ROTATION;
+//         char = String.fromCharCode(overflowASCII);
+//       } else if (char.match(/[A-Za-z]/)) { 
+//         char = String.fromCharCode(currCharASCII + ROTATION);
+//       }
+
+//     stringArr.push(char)
+//     });
+  
+//   return stringArr.join('');
+// }
+
 const UPPERCASE_ASCII_MID = 78;
 const LOWERCASE_ASCII_MID = 110;
 const ALPHABET_LENGTH = 26;
@@ -11,7 +39,7 @@ function rot13(string) {
     let overflowASCII;
 
     if (char.match(/[A-Z]/) && currCharASCII >= UPPERCASE_ASCII_MID) { 
-      overflowASCII = (currCharASCII - ALPHABET_LENGTH) + ROTATION;
+        overflowASCII = (currCharASCII - ALPHABET_LENGTH) + ROTATION;
         char = String.fromCharCode(overflowASCII);
       } else if (char.match(/[a-z]/) && currCharASCII >= LOWERCASE_ASCII_MID) { 
         overflowASCII = (currCharASCII - ALPHABET_LENGTH) + ROTATION;
@@ -22,7 +50,7 @@ function rot13(string) {
 
     stringArr.push(char)
     });
-  
+
   return stringArr.join('');
 }
 
